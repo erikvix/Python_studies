e44173f8ea13bfe494f80b7ab41bd4f3b1c36bbd
erikvix <erikvixx@gmail.com>
2023-04-19 04:46:15 -0300

    studie

--- logic_python/ex104.py
+++ logic_python/ex104.py
@@ -1,8 +1,10 @@
 def notas(*num):
     sit = False
     val = {}
-    val['total'] = num + num
-    print(val, total)
+    val['total'] = max(num)
+    # val['menor'] = min(num)
+    # val['maior'] = max(num)
+    print(val)
 
 
 notas(3, 10, 5)
