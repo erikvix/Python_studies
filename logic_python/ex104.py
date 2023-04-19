6f49359290c84293c067c089abe858daa6bf0ff3
erikvix <erikvixx@gmail.com>
2023-04-19 11:44:05 -0300

    studies

--- logic_python/ex104.py
+++ logic_python/ex104.py
@@ -1,10 +1,18 @@
-def notas(*num):
-    sit = False
+def notas(*num, sit=False):
     val = {}
-    val['total'] = max(num)
-    # val['menor'] = min(num)
-    # val['maior'] = max(num)
-    print(val)
+    val['total'] = len(num)
+    val['menor'] = min(num)
+    val['maior'] = max(num)
+    val['media'] = sum(num) / len(num)
+    if sit == True:
+        if val['media'] >= 6:
+            val['situação'] = 'BOA'
+        elif val['media'] >= 5:
+            val['situação'] = 'RAZOÁVEL'
+        else:
+            val['situação'] = 'RUIM'
+    return val
 
 
-notas(3, 10, 5)
+resp = notas(3, 8, 5, 6, sit=True)
+print(resp)
